d060254456f3450a73624ee7a6848c8d41182cf1
chavarahul <chavarahul7@gmail.com>
2024-10-06 00:03:30 +0530

    protected route

--- app/(sites)/dashboard/page.tsx
+++ app/(sites)/dashboard/page.tsx
@@ -1,9 +1,15 @@
+import { auth } from '@/app/lib/auth'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+const Dashboard = async () => {
+    const session = await auth();
+    if(!session?.user){
+        return redirect("/")
+    }
+    return (
+        <div>Dashboard</div>
+    )
 }
 
 export default Dashboard
